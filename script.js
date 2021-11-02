d8b72e1cdac6b2aecc14c7eb7fbaa5e87b4d19d6
jessica <jessicamolin@hotmail.com>
2021-11-02 21:38:19 +0100

    lade till onSubmit-alert submitted

--- script.js
+++ script.js
@@ -56,6 +56,9 @@ function button(){
 
 button();
 
+function onSubmit(){
+    alert("submitted")
+}
  const  valid = document.querySelectorAll('.validation')
     valid.forEach(element=> {
         
