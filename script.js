6843b4942fd13957879bda819b698ef93379e6bd
jessica <jessicamolin@hotmail.com>
2021-11-04 18:57:51 +0100

    klar

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 function minValue(str,minValue){
-    if(str.trim().length >= minValue)
-        return true
-    
+    let strLengt=str.trim();
+    let expression ='^.{' + minValue + ',35}$';
+    const r= new RegExp(expression);
+
+    if(r.test(strLengt))
+        return true 
 }
 
 function validateZipCode(str){
     const r = /\b\d{5}\b/g;
     const t= /^\d{3} \d{2}$/
-        if( r.test(str) || t.test(str)) 
-            return true
-        
+
+    if( r.test(str) || t.test(str)) 
+        return true     
 }
 
 function validateAge(dateOfBirth){
@@ -32,32 +35,28 @@ function validateEmail(str){
         
 }
 function comparePassword(str,password){
-    if(str.trim() === password){
+    if(str.trim() === password)
       
         return true
-    }
+    
 }
 
-// function strongPassword(str){
-//     const r= /(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-//     if(r.test(str)){
-//        document.getElementById('strongPassword').innerHTML ="strong pasword";
-//        alert("bra")
-//     }
-
-// }
+function strongPassword(str){
+    const strong= /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+    if(strong.test(str)){
+       return true
+    }
+}
 
 function button(){
     const button =document.querySelector('button');
     button.disabled=true;
     const exist=document.querySelectorAll('.button-off')
-    console.log(exist)
+
     if(exist.length>0){
         button.disabled=true
-        console.log(button.disabled)
     }else {
         button.disabled=false
-        console.log(button.disabled)
     }
 }
 
@@ -69,134 +68,107 @@ function onSubmit(){
     alert("submitted")
 }
  const  valid = document.querySelectorAll('.validation')
-    valid.forEach(element=> {
-        
-        // console.log(element)
+    valid.forEach(element=> { 
         if(element.type==="text"){
             element.addEventListener("keyup", function(e) {
                 if (!minValue(e.target.value,"2")){
                     e.target.classList.add("button-off")
                     e.target.classList.add("is-invalid")
                     button();
-                   }
-                else {
-                    
+                   }else {
                     e.target.classList.remove("is-invalid")
                     e.target.classList.add("is-valid")  
                     e.target.classList.remove("button-off")
                     button();
-                }
-                    
-                     
+                }      
             })
         }
         if(element.type==="email"){
             element.addEventListener("keyup", function(e){
                 if(!validateEmail(e.target.value))
-                    {
-                        
+                    {   
                     e.target.classList.add("button-off")
                     e.target.classList.add("is-invalid")
                     button();
-                    }
-                else {
-                    
+                    } else {
                     e.target.classList.remove("is-invalid")
                     e.target.classList.add("is-valid")
                     e.target.classList.remove("button-off")
                     button();
                 }
-                
-                
             })
           }
           
         if(element.id ==="inputZip"){
             element.addEventListener("keyup", function(e){
-                if(!validateZipCode(e.target.value))
-                {
-                    
+                if(!validateZipCode(e.target.value)) {
                     e.target.classList.add("button-off")
                     e.target.classList.add("is-invalid")
                     button();
-                }
-                else {
-                    
+                } else {
                     e.target.classList.remove("is-invalid")
                     e.target.classList.add("is-valid")
                     e.target.classList.remove("button-off")
                     button();
-                }
-               
-                
-                
+                } 
             })
           }
         if(element.id ==="Birtdate"){
             element.addEventListener("keyup", function(e){
-                if(!validateAge(e.target.value))
-                {
-                    
+
+                if(!validateAge(e.target.value)){
                     e.target.classList.add("button-off")
                     e.target.classList.add("is-invalid")
                     button();
-                }
-                
-                else {
-                    
+                }else {
                     e.target.classList.remove("is-invalid")
                     e.target.classList.add("is-valid") 
                     e.target.classList.remove("button-off")
                     button();
-                }
-                
-                
+                }  
             })
           }
         
         if (element.id==="inputPassword"){
             element.addEventListener("keyup", function(e){
-                if(!minValue(e.target.value,"8"))
-                {
-                    
+                if(!minValue(e.target.value,"8")){
+                    document.querySelector('.passwordStrong').innerHTML="";
+                    document.querySelector('.passwordWeek').innerHTML="";
                     e.target.classList.add("button-off")
                     e.target.classList.add("is-invalid")
                     button();
-                
-                }
-                else  {
+                }else  {
                     e.target.classList.remove("is-invalid")
                     e.target.classList.add("is-valid")
                     e.target.classList.remove("button-off")
                     button();
-                
-                }
-                
-                
+
+                    if(strongPassword(e.target.value)){
+                        document.querySelector('.passwordWeek').innerHTML="";
+                        document.querySelector('.passwordStrong').innerHTML="Password strength: Strong";  
+                    }else {
+                        document.querySelector('.passwordStrong').innerHTML="";
+                        document.querySelector('.passwordWeek').innerHTML="Password strength: Week";
+                    }
+                }  
             })
           }
-        //   []
-        if (element.id ==="inputPassword2"){
-                const passwordelement=document.querySelector("#inputPassword");
-                const password=passwordelement;
-                element.addEventListener("keyup", function(e){
-                    if(!comparePassword(e.target.value,password.value))
-                    {
-                        e.target.classList.add("button-off")
-                        e.target.classList.add("is-invalid")
-                        button();
-                    }
-                    else  {
-                        
-                        e.target.classList.remove("button-off")
-                        e.target.classList.remove("is-invalid")
-                        e.target.classList.add("is-valid")
-                        button();
-                    }
 
-                })
-        }
-        // []
-        // button();
-        
+        if (element.id ==="inputPassword2"){
+            const passwordelement=document.querySelector("#inputPassword");
+            const password=passwordelement;
+            element.addEventListener("keyup", function(e){
+                 if(!comparePassword(e.target.value,password.value))
+                 {
+                     e.target.classList.add("button-off")
+                     e.target.classList.add("is-invalid")
+                    button();
+                }else  {
+                     e.target.classList.remove("button-off")
+                    e.target.classList.remove("is-invalid")
+                    e.target.classList.add("is-valid")
+                     button();
+                }
+            })
+        }  
     })
